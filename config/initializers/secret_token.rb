5b3f88c5418e879723ded64d0affbda086e61f65
pasha <p.korenev@voroninstudio.eu>
2014-02-21 16:30:14 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwoHeartsNew::Application.config.secret_key_base = '9073ad82fcaef1169ccf46d79df2977dad3d7e0e48fcf8f4b62b5cfeb12abadc8f36a475890b14cc2085caa8e96ac079dfa723166c70acb2513bac72e26c828c'
