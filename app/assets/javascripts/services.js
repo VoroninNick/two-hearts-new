09b9efab0da7229fdc27c1ea1122ef395489c17d
pasha <p.korenev@voroninstudio.eu>
2014-03-27 16:38:46 +0200

    services

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -8,61 +8,16 @@ $(document).ready(function() {
         return false;
     });
 
-    $("#show-menu-13").click(function(event){
-
-        if(window.location.pathname == "/services/spetsefekti/zvuk-svitlo" || window.location.pathname == "/services/spetsefekti/pirotehnika" || window.location.pathname == "/services/spetsefekti/novaya-sab-kategoriya" || window.location.pathname == "/services/spetsefekti/Low-fog"){
-            //event.preventDefault();
-        }else{
-            $("#show-menu-13 ul").slideToggle("slow", function(){});
-            $(this).toggleClass("sub-active");
-
-            $("#show-13").each(function (i) {
-                if($(this).text() != "-"){
-                    $("#show-13").text("-");
-                }else{
-                    $("#show-13").text("+");
-                }
-            });
-        }
-
-    });
-
-    $("#show-menu-16").click(function(event){
-
-        if(window.location.pathname == "/services/shou-programi/shou-baleti" || window.location.pathname == "/services/shou-programi/zirki-estradi" || window.location.pathname == "/services/shou-programi/artisti"){
-            //event.preventDefault();
-        }else{
-            $("#show-menu-16 ul").slideToggle("slow", function(){});
-            $(this).toggleClass("sub-active");
-
-            $("#show-16").each(function (i) {
-                if($(this).text() != "-"){
-                    $("#show-16").text("-");
-                }else{
-                    $("#show-16").text("+");
-                }
-            });
-        }
-
-    });
-
-    $("#show-menu-17").click(function(event){
-
-        if(window.location.pathname == "/services/eksklyuziv/zaproshennya" ){
-            //event.preventDefault();
-        }else{
-            $("#show-menu-17 ul").slideToggle("slow", function(){});
-            $(this).toggleClass("sub-active");
-
-            $("#show-17").each(function (i) {
-                if($(this).text() != "-"){
-                    $("#show-17").text("-");
-                }else{
-                    $("#show-17").text("+");
-                }
-            });
-        }
-
+    $('#services-page').each(function(){
+        $('#catalog-list > ul > li > span ').click(function(){
+           var $link = $(this);
+           var $li = $link.parent();
+           $li = $($li);
+            var $sub_ul = $li.find('> ul');
+            if($sub_ul.length > 0){
+                $sub_ul.css({display:'block'});
+            }
+        });
     });
 
 });
